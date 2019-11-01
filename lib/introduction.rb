ddf1a5ba89eeae6436dff327073d667c7fd65feb
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-01 00:04:41 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,18 +8,30 @@ def introduction(name) {
   return "Hi, my name is #{name}.";
 }
 
-# Test 2;
+# Test 2; return should be puts for Ruby!
 def introduction_with_language(name, language) {
-  return  "Hi, my name is #{name} and I am learning to program in #{language}."
+  return  "Hi, my name is #{name} and I am learning to program in #{language}.";
 }
 
 # Test 3;
 def introduction_with_language_optional(name, language = "Ruby") {
-  return "Hi, my name is #{name} and I am learning to program in #{language}."
+  return "Hi, my name is #{name} and I am learning to program in #{language}.";
 }
 
 
+# Correct Solution...
 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
